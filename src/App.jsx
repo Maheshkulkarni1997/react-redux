9c733fdb42c0c6d345b4acf7d564e53a60d866bb
Mahesh <maheshkulkarni9797@gmail.com>
2025-11-21 12:46:30 +0530

    remove unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Counter from './Counter'
 import ProductList from './features/products/ProductList'
-import Cart from './components/Cart'
 import CartIcon from './components/CartIcon'
 
 function App() {
